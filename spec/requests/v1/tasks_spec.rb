ab0bce37bd733cdac1ae5e05700d2aa81193ca97
Pablo Carbajal <pablo@cover.com>
2019-05-29 22:09:56 -0400

    Added expected response from Postman GET api/v1/tasks

--- spec/requests/v1/tasks_spec.rb
+++ spec/requests/v1/tasks_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'V1::Tasks', type: :request do
   describe 'GET /v1/tasks' do
-    it 'works! (now write some real specs)' do
+    let(:postman_get_tasks_json_response) { parse_json file_fixture('v1/postman_get_tasks.json').read }
+
+    it 'matches Postman example "Existing Title check"' do
       get v1_tasks_path
+
       expect(response).to have_http_status(200)
+      expect(parse_json response.body).to eq(postman_get_tasks_json_response)
     end
   end
 end
