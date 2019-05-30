fae591a5daf25c25ec6c9682306b5752619b9468
Pablo Carbajal <pablo@cover.com>
2019-05-29 23:24:14 -0400

    Added request and expected response from Postman POST api/v1/tasks and renamed fixtures for clarity

--- spec/requests/v1/tasks_spec.rb
+++ spec/requests/v1/tasks_spec.rb
@@ -2,13 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'V1::Tasks', type: :request do
   describe 'GET /v1/tasks' do
-    let(:postman_get_tasks_json_response) { parse_json file_fixture('v1/postman_get_tasks.json').read }
+    let(:json_response) { parse_json file_fixture('v1/postman_get_tasks_response.json').read }
 
-    it 'matches Postman example "Existing Title check"' do
+    it 'matches Postman example' do
       get v1_tasks_path
 
       expect(response).to have_http_status(200)
-      expect(parse_json response.body).to eq(postman_get_tasks_json_response)
+      expect(parse_json response.body).to eq(json_response)
+    end
+  end
+
+  describe 'POST /v1/tasks' do
+    let(:json_request) { parse_json file_fixture('v1/postman_post_tasks_request.json').read }
+    let(:json_response) { parse_json file_fixture('v1/postman_post_tasks_response.json').read }
+
+    it 'matches Postman example' do
+      post v1_tasks_path, params: json_request
+
+      expect(response).to have_http_status(200)
+      expect(parse_json response.body).to eq(json_response)
     end
   end
 end
