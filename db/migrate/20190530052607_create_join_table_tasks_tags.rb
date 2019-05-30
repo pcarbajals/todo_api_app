6947b58326939810ac6a1c197570ae40cdb61675
Pablo Carbajal <pablo@cover.com>
2019-05-30 01:53:35 -0400

    Implemented has_and_belongs_to_many relationship between Tasks and Tags

---
+++ db/migrate/20190530052607_create_join_table_tasks_tags.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableTasksTags < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :tasks, :tags do |t|
+      t.belongs_to :task, index: true
+      t.belongs_to :tag, index: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_031651) do
+ActiveRecord::Schema.define(version: 2019_05_30_052607) do
 
   create_table "tags", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_05_29_031651) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_tags_on_task_id"
+  end
+
+  create_table "tags_tasks", id: false, force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "tag_id"
+    t.index ["tag_id"], name: "index_tags_tasks_on_tag_id"
+    t.index ["task_id"], name: "index_tags_tasks_on_task_id"
   end
 
   create_table "tasks", force: :cascade do |t|
