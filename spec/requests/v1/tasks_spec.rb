662f62390367d4375c10654e6a2e0cd7147d997b
Pablo Carbajal <pablo@cover.com>
2019-05-31 01:29:08 -0400

    Reformatted

--- spec/requests/v1/tasks_spec.rb
+++ spec/requests/v1/tasks_spec.rb
@@ -6,40 +6,40 @@ RSpec.describe 'V1::Tasks', type: :request do
       task = create(:task_wash_laundry)
       task.tags << create(:tag_today)
     end
-    
+
     context 'GET /v1/tasks' do
       let(:json_response) { parse_json file_fixture('v1/postman_get_tasks_response.json').read }
-  
+
       it 'matches example' do
         get v1_tasks_path
-  
+
         expect(response).to have_http_status(200)
         expect(parse_json response.body).to eq(json_response)
       end
     end
-  
+
     context 'POST /v1/tasks' do
       let(:json_request) { parse_json file_fixture('v1/postman_post_tasks_request.json').read }
       let(:json_response) { parse_json file_fixture('v1/postman_post_tasks_response.json').read }
-  
+
       it 'matches example' do
         post v1_tasks_path, params: json_request
-  
+
         expect(response).to have_http_status(201)
         expect(parse_json response.body).to eq(json_response)
       end
     end
-  
+
     context 'PATCH /v1/tasks/2' do
       let(:json_request) { parse_json file_fixture('v1/postman_post_tasks_2_request.json').read }
       let(:json_response) { parse_json file_fixture('v1/postman_post_tasks_2_response.json').read }
       let(:task_id) { json_request['data']['id'] }
-  
+
       it 'matches example without tags' do
         create(:task_wash_laundry, id: task_id)
 
         patch v1_task_path(task_id), params: json_request
-  
+
         expect(response).to have_http_status(200)
         expect(parse_json response.body).to eq(json_response)
       end
