627c6d1986f442615c332a0ce6f673b6115c9543
Pablo Carbajal <pablo@cover.com>
2019-05-31 01:38:56 -0400

    Implemented GET /v1/tags test case

--- spec/requests/v1/tags_spec.rb
+++ spec/requests/v1/tags_spec.rb
@@ -1,41 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe 'V1::Tags', type: :request do
-  describe 'GET /v1/tags' do
-    let(:json_response) { parse_json file_fixture('v1/postman_get_tags_response.json').read }
+  describe 'Postman integration tests' do
+    before(:all) do
+      task_one = create(:task, id: 1)
+      task_two = create(:task, id: 2)
+      
+      tag_today = create(:tag_today)
+      tag_today.tasks << task_one
+      
+      tag_urgent = create(:tag, title: 'Urgent')
+      tag_urgent.tasks << task_two
 
-    it 'matches example' do
-      get v1_tags_path
-
-      expect(response).to have_http_status(200)
-      expect(parse_json response.body).to eq(json_response)
+      tag_home = create(:tag, title: 'Home')
+      tag_home.tasks << task_two
     end
-  end
 
-  context 'POST /v1/tags' do
-    let(:json_request) { parse_json file_fixture('v1/postman_post_tags_request.json').read }
-    let(:json_response) { parse_json file_fixture('v1/postman_post_tags_response.json').read }
-
-    it 'matches example' do
-      post v1_tags_path, params: json_request
-
-      expect(response).to have_http_status(201)
-      expect(parse_json response.body).to eq(json_response)
+    context 'GET /v1/tags' do
+      let(:json_response) { parse_json file_fixture('v1/postman_get_tags_response.json').read }
+  
+      it 'matches example' do
+        get v1_tags_path
+  
+        expect(response).to have_http_status(200)
+        expect(parse_json response.body).to eq(json_response)
+      end
     end
-  end
-
-  context 'PATCH /v1/tags/2' do
-    let(:json_request) { parse_json file_fixture('v1/postman_post_tags_2_request.json').read }
-    let(:json_response) { parse_json file_fixture('v1/postman_post_tags_2_response.json').read }
-    let(:tag_id) { json_request['data']['id'] }
-
-    it 'matches example' do
-      # create(:task_wash_laundry, id: task_id)
-
-      patch v1_tag_path(tag_id), params: json_request
-
-      expect(response).to have_http_status(200)
-      expect(parse_json response.body).to eq(json_response)
+  
+    context 'POST /v1/tags' do
+      let(:json_request) { parse_json file_fixture('v1/postman_post_tags_request.json').read }
+      let(:json_response) { parse_json file_fixture('v1/postman_post_tags_response.json').read }
+  
+      it 'matches example' do
+        post v1_tags_path, params: json_request
+  
+        expect(response).to have_http_status(201)
+        expect(parse_json response.body).to eq(json_response)
+      end
+    end
+  
+    context 'PATCH /v1/tags/2' do
+      let(:json_request) { parse_json file_fixture('v1/postman_post_tags_2_request.json').read }
+      let(:json_response) { parse_json file_fixture('v1/postman_post_tags_2_response.json').read }
+      let(:tag_id) { json_request['data']['id'] }
+  
+      it 'matches example' do
+        # create(:task_wash_laundry, id: task_id)
+  
+        patch v1_tag_path(tag_id), params: json_request
+  
+        expect(response).to have_http_status(200)
+        expect(parse_json response.body).to eq(json_response)
+      end
     end
   end
 end
