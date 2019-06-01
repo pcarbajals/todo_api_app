7fcd76da0686f38eeb627bab6ef2802a6f74f2eb
Pablo Carbajal <pablo@cover.com>
2019-06-01 03:15:04 -0400

    Validates presence of attribute title

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
+  validates_presence_of :title
   has_and_belongs_to_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  validates_presence_of :title
   has_and_belongs_to_many :tags
 end
