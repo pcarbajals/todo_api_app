cd6eaad92abf284ca1d4377837aa379a5055eaa9
Pablo Carbajal <pablo@cover.com>
2019-06-01 14:08:26 -0400

    Adds tests for validations and associations

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations and associations' do
+    it { should validate_presence_of :title }
+    it { should have_and_belong_to_many :tags }
+  end
 end
