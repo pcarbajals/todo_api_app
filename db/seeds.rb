260fc0852f90ee677fd0e0effb73aafdc59427bd
Pablo Carbajal <pablo@cover.com>
2019-06-01 15:29:32 -0400

    Seeds database for Postman Collection Runner

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Task.create(title: 'Sample Task')
+task = Task.create(id: 0, title: 'Wash laundry')
+task.tags << Tag.create(title: 'Today')
+Task.create(id: 2, title: 'Patch this task')
